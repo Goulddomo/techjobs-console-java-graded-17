49b53023be6fee8df18fb528df80ee78b97362c8
Dominique Gould <dominiquegould42@yahoo.com>
2024-04-15 18:59:06 -0500

    Test 1 Done

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
         for (HashMap<String,String> job : someJobs) {
+            System.out.println();
             System.out.println("*****");
             for (Map.Entry<String, String> entry: job.entrySet()){
                 System.out.println(entry.getKey() + ": " + entry.getValue());
